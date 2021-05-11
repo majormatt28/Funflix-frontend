298ebad49956863337b2a61f61d2c409759a4c72
Matthew <elijah2895@hotmail.com>
2021-05-11 13:54:20 -0400

    more styling

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -15,15 +15,12 @@ function MovieDetail(){
     },[id])
 
     const {title, image, release_date, description} = movie
-    const renderReview=()=>movie.reviews.map(review=><p key={review.id}>Post by: {review.username} Comment: {review.comment} </p>)
-       
-    
-     
+    const renderReview=()=> movie.reviews.map(review=><p key={review.id}>Post by: {review.username} Comment: {review.comment} </p>)
 
     return (
         <div>
             <h1>{title}</h1>
-            <img src={image} alt={title}/>
+            <img className="movie-poster" src={image} alt={title}/>
             <h4>{description}</h4>
             <h4>Release Date: {release_date}</h4>
             {movie.reviews ? renderReview() : <p>There is no comment yet</p>}
